4b3be811308ec8f7dee5384fbc5ce1898330cee2
wz <17600820823@163.com>
2023-09-27 09:21:19 +0800

    feat: 删除openapi相关

--- config/config.ts
+++ config/config.ts
@@ -139,18 +139,6 @@ export default defineConfig({
    * @doc https://pro.ant.design/zh-cn/docs/openapi/
    */
   openAPI: [
-    {
-      requestLibPath: "import { request } from 'umi'",
-      // 或者使用在线的版本
-      // schemaPath: "https://gw.alipayobjects.com/os/antfincdn/M%24jrzTTYJN/oneapi.json"
-      schemaPath: join(__dirname, 'oneapi.json'),
-      mock: false,
-    },
-    {
-      requestLibPath: "import { request } from 'umi'",
-      schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/CA1dOm%2631B/openapi.json',
-      projectName: 'swagger',
-    },
   ],
   mfsu: {
     strategy: 'normal',
