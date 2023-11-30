202f1b995b4cfc1eb7d81eb28bec5c4783db4969
wz <17600820823@163.com>
2023-11-30 17:46:43 +0800

    feat: 创建工作流  公共部分完成 私有部分开始

---
+++ src/pages/Flow/components/CreateFlowRead.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface IProps {
+  [key: string]: any;
+}
+
+const CreateFlowRead: React.FC<IProps> = (props) => {
+  const {} = props;
+  return (
+    <>
+      CreateFlowRead
+    </>
+  );
+};
+
+export default CreateFlowRead;
--- src/pages/Flow/components/CreateMethodItem.tsx
+++ src/pages/Flow/components/CreateMethodItem.tsx
@@ -4,6 +4,7 @@ import { Card, Col, Form, Input, Row, Select, Spin } from 'antd';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useModel } from 'umi';
+import CreateFlowRead from "@/pages/Flow/components/CreateFlowRead";
 
 interface IProps {
   form: FormInstance<any>;
@@ -105,6 +106,77 @@ const CreateMethodItem: React.FC<IProps> = (props) => {
       </Row>
 
       {/*以下都是选择指令之后展示的内容*/}
+
+      {/*读值*/}
+      {method === 'read' && <CreateFlowRead name={name}/>}
+
+      {/*写值*/}
+      {method === 'read' && <></>}
+
+      {/*等待*/}
+      {method === 'sleep' && <></>}
+
+      {/*移动*/}
+      {method === 'methdMvThing' && <></>}
+
+      {/*加液*/}
+      {method === 'methdAddSol' && <></>}
+
+      {/*开瓶器位开盖*/}
+      {method === 'methdUncap' && <></>}
+
+      {/*开瓶器位加盖*/}
+      {method === 'methdRecap' && <></>}
+
+      {/*换工具*/}
+      {method === 'methdChngTool' && <></>}
+
+      {/*抬起枪头*/}
+      {method === 'methdPickTip' && <></>}
+
+      {/*开瓶器工位吸液，滴加位吐液*/}
+      {method === 'methdTrnsLiquid' && <></>}
+
+      {/*移固体料仓*/}
+      {method === 'methdMvSolid' && <></>}
+
+      {/*加固启动*/}
+      {method === 'methdDoAddSolid' && <></>}
+
+      {/*蠕动泵加液*/}
+      {method === 'methdDoPeristaltic' && <></>}
+
+      {/*搅拌3*/}
+      {method === 'methdDoMix3' && <></>}
+
+      {/*废液蒸馏*/}
+      {method === 'methdDoDistillC3' && <></>}
+
+      {/*以下不展示*/}
+      {
+        0 > 1 && <>
+          {/*行星搅拌*/}
+          {method === 'methdDoMix3Planet' && <>行星搅拌</>}
+
+          {/*安装搅拌桨*/}
+          {method === 'methdGetPaddle' && <>安装搅拌桨</>}
+
+          {/*卸载搅拌桨*/}
+          {method === 'methdPutPaddle' && <>卸载搅拌桨</>}
+
+          {/*"搅拌2*/}
+          {method === 'methdDoMix2' && <>"搅拌2</>}
+
+          {/*设置冷凝管温度*/}
+          {method === 'methdSetCooler' && <>设置冷凝管温度</>}
+
+          {/*"设置加热带温度*/}
+          {method === 'methdSetHeatBand' && <>"设置加热带温度</>}
+
+          {/*"设置磁力搅拌加热器*/}
+          {method === 'methdSetStir' && <>"设置磁力搅拌加热器</>}
+        </>
+      }
 
 
     </Card>
