34ffc888f09f67d380ce1c03b77090b230b8679e
wz <17600820823@163.com>
2023-12-12 11:08:56 +0800

    feat: 上传时候添加升温曲线 背温曲线表头

--- src/api/others.ts
+++ src/api/others.ts
@@ -11,6 +11,8 @@ export const uploadAnnex = ( data: Other.UploadExpAnnex) => {
   formData.append('name', data.name);
   formData.append('file', data.file);
   formData.append('description', data.description);
+  formData.append('y_axis_front', data.y_axis_front);
+  formData.append('y_axis_back', data.y_axis_back);
   return request<Response<any>>(`/api/v1/expt/expts/attachments/upload`, {
     method: 'post',
     requestType: 'form',
