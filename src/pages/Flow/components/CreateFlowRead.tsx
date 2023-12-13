3ac3277b2a92db0df42e351ad5a29c33c02eb43b
wz <17600820823@163.com>
2023-12-13 14:38:36 +0800

    feat: 工作流 读值

--- src/pages/Flow/components/CreateFlowRead.tsx
+++ src/pages/Flow/components/CreateFlowRead.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
+import { Col, Form, Input, Row, Select } from "antd";
+import { IForm } from "@/pages/typings";
+import { useModel } from "@@/exports";
 
 interface IProps {
+  name: number;
+
   [key: string]: any;
 }
 
+const formRules: IForm.IFormRules = {
+  node_index: [{ required: true, message: '请选择节点' }],
+};
 const CreateFlowRead: React.FC<IProps> = (props) => {
-  const {} = props;
+  const { name } = props;
+  const { nodeIds } = useModel('useExperimentModel');
+
   return (
     <>
-      CreateFlowRead
+      <Row gutter={10}>
+        <Col span={12}>
+          <Form.Item name={[ name, 'node_index' ]} label="节点id" rules={formRules.node_index}>
+            <Select
+              allowClear
+              options={nodeIds}
+              placeholder="请选择节点"
+            />
+          </Form.Item>
+        </Col>
+      </Row>
     </>
-  );
+  )
+    ;
 };
 
 export default CreateFlowRead;
